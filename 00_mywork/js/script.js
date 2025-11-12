ea258ed3e4e3888c05747ffbb21f94b68e2d3638
okayu1819 <yktruth15@gmail.com>
2025-11-13 04:42:59 +0900

    fix nav list and scroll animation

--- 00_mywork/js/script.js
+++ 00_mywork/js/script.js
@@ -1,20 +1,20 @@
 $(function () {
-
   //ページ内スクロール
-  var navHeight = $(".gnav").outerHeight();
+  var navHeight = $(".c-nav").outerHeight();
 
   $('a[href^="#"]').on("click", function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top - navHeight;
-    $(".mc").animate({ scrollTop: position, }, 800, "swing");
+    
+    // メインコンテンツ要素をスクロール
+    $(".p-mc").animate({ scrollTop: position }, 800, "swing");
     return false;
   });
 
   //ページトップ
   $("#js-page-top").on("click", function () {
-    $(".mc").animate({ scrollTop: 0, }, 800);
+    $(".p-mc").animate({ scrollTop: 0 }, 800);
     return false;
   });
-
 });
